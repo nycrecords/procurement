67355685da7208c7dece0045e79b8de3c4198b24
Gary Zhou <garyzhouny@gmail.com>
2016-04-22 11:07:34 -0400

    Added error flashing for each question

--- app/forms.py
+++ app/forms.py
@@ -30,15 +30,21 @@ class NewRequestForm(Form):
 		request_name = StringField(u'Name', validators=[
 				DataRequired('Please enter the requestor\'s name')])
 		request_division = SelectField(u'Division*', choices=divisions,
-																	 validators=[DataRequired(
-																			 'The request division is required')])
+				validators=[DataRequired('Please select the division')], default='')
 		request_item = TextAreaField(u'Item*(required)', validators=[
-			DataRequired('You must enter a FULL item ''description of your request'),
-			Length(1, 250,'Your request summary must be less than 250 characters')])
+				DataRequired('You must enter a FULL item description of your request'),
+				Length(1, 100,'The item description must be less than 100 characters')])
 		request_quantity = StringField(u'Quantity*', validators=[
 				DataRequired('Please enter the quantity')])
+		request_price_per_item = StringField(u'Price per item*', validators=[
+				DataRequired('Please enter the price per item')])
+		request_total = StringField(u'Total price*', validators=[
+				DataRequired('Please enter the total price')])
 		request_funding_source = SelectField(u'Funding*', choices=funding,
-																		validators=[DataRequired(
-																				'The request funding source is required')])  
+				validators=[DataRequired('Please select the funding source')])
+		request_funding_other = StringField(u'Funding Other')
+		request_justification = TextAreaField(u'Justification*(required)', validators=[
+				DataRequired('You must enter a justification for your request'),
+				Length(1, 255,'The justification must be less than 255 characters')])  
 		submit = SubmitField(u'Submit Request')  
 
