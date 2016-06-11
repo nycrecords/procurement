437f26bc36383e47e8d17b57c1777fe1414de391
Joel Castillo <joel.castillo@nyu.edu>
2016-06-10 20:01:33 -0400

    Initial functionality for editing a request.
    TODO: Comments
    TODO: Edit Forms

--- app/request/forms.py
+++ app/request/forms.py
@@ -1,9 +1,13 @@
-"""Forms related to a specific request."""
-from flask_wtf import Form
-from flask_wtf.file import FileField
-from wtforms import StringField, SelectField, TextAreaField, RadioField, \
-    DecimalField, IntegerField
+"""
+.. module:: forms.
 
+   :synopsis: Defines edit forms for Procurement requests.
+"""
+from flask_wtf import Form
+from wtforms import StringField, SelectField, TextAreaField, SubmitField, \
+     RadioField, DecimalField, IntegerField
+from wtforms.validators import DataRequired, Length, Optional
+from wtforms_alchemy import PhoneNumberField
 
 funding = [
     ('', ''),
@@ -15,28 +19,32 @@ funding = [
 ]
 
 
-class EditRequestForm(Form):
-    """A form for editing an existing request"""
+class NewRequestForm(Form):
+    '''A form class for procurement new purchase requests'''
+    request_name = StringField(u'Name*(required)', validators=[
+        DataRequired('Please enter the requestor\'s name'), Length(1, 100)])
+    division = SelectField(u'Division*', choices=divisions,
+                           validators=[DataRequired('Please select the division')], default='')
     item = TextAreaField(u'Item*(required)', validators=[
-    DataRequired('You must enter a FULL item description of your request'),
-    Length(1, 100, 'The item description must be less than 100 characters')])
-quantity = IntegerField(u'Quantity*', validators=[
-    DataRequired('Please enter the quantity')])
-unit_price = DecimalField(u'Price per item*', validators=[
-    DataRequired('Please enter the price per item')])
-total_cost = DecimalField(u'Total price*', validators=[
-    DataRequired('Please enter the total price')])
-funding_source = SelectField(u'Funding*', choices=funding,
-                             validators=[DataRequired('Please select the funding source')])
-funding_source_description = StringField(u'Funding Other')
-justification = TextAreaField(u'Justification*(required)', validators=[
-    DataRequired('You must enter a justification for your request'),
-    Length(1, 255, 'The justification must be less than 255 characters')])
-request_vendor_name = StringField(u'Vendor Name')
-request_vendor_address = StringField(u'Vendor Address')
-request_vendor_phone = PhoneNumberField(region='US', display_format='national')
-request_vendor_fax = PhoneNumberField(region='US', display_format='national')
-request_vendor_email = StringField(u'Email')
-request_vendor_taxid = StringField(u'Vendor Tax ID')
-request_MWBE = RadioField(u'MWBE', choices=[('True', 'Yes'), ('False', 'No')], validators=[Optional()])
-submit = SubmitField(u'Submit Request')
+        DataRequired('You must enter a FULL item description of your request'),
+        Length(1, 100, 'The item description must be less than 100 characters')])
+    quantity = IntegerField(u'Quantity*', validators=[
+        DataRequired('Please enter the quantity')])
+    unit_price = DecimalField(u'Price per item*', validators=[
+        DataRequired('Please enter the price per item')])
+    total_cost = DecimalField(u'Total price*', validators=[
+        DataRequired('Please enter the total price')])
+    funding_source = SelectField(u'Funding*', choices=funding,
+                                 validators=[DataRequired('Please select the funding source')])
+    funding_source_description = StringField(u'Funding Other')
+    justification = TextAreaField(u'Justification*(required)', validators=[
+        DataRequired('You must enter a justification for your request'),
+        Length(1, 255, 'The justification must be less than 255 characters')])
+    request_vendor_name = StringField(u'Vendor Name')
+    request_vendor_address = StringField(u'Vendor Address')
+    request_vendor_phone = PhoneNumberField(region='US', display_format='national')
+    request_vendor_fax = PhoneNumberField(region='US', display_format='national')
+    request_vendor_email = StringField(u'Email')
+    request_vendor_taxid = StringField(u'Vendor Tax ID')
+    request_MWBE = RadioField(u'MWBE', choices=[('True', 'Yes'), ('False', 'No')], validators=[Optional()])
+    submit = SubmitField(u'Submit Request')
