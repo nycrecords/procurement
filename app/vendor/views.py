628c9c896b0ab65b67fdfe4495df3ddec3831c1b
Joel Castillo <joel.castillo@nyu.edu>
2016-06-21 21:42:00 -0400

    Added API endpoint for editing / creating a vendor

--- app/vendor/views.py
+++ app/vendor/views.py
@@ -8,8 +8,6 @@ from flask import render_template, request, abort, redirect, url_for
 from .. import db
 from ..models import Request, User, Vendor, Comment
 from . import vendor as vendor_blueprint
-from ..main.forms import RequestForm
-from .forms import EditVendorForm
 
 
 @vendor_blueprint.route('/<vendor_id>', methods=['GET'])
@@ -28,24 +26,16 @@ def view_vendor(vendor_id):
         abort(404)
 
 
-# TODO: Edit Vendor API Endpoint
-@vendor_blueprint.route('/edit/<vendor_id>',
-                        methods=['GET', 'POST'])
+@vendor_blueprint.route('/edit',
+                        methods=['POST'])
 def edit_vendor(vendor_id):
     """Edit a vendor."""
-    if request.method == 'GET':
-        vendor = Vendor.query.filter_by(id=vendor_id).first()
-        if vendor:
-            form = EditVendorForm(obj=vendor)
-        else:
-            form = EditVendorForm()
-        return render_template('vendor/edit_vendor.html',
-                               vendor=vendor,
-                               form=form)
-        # TODO: Implement GET handler
-    elif request.method == 'POST':
-        return str(vendor)
-        # TODO: Implement POST handler
-    else:
-        return redirect(url_for('vendor.new_vendor'))
-        # abort(400)
+    if not request.json or 'name' not in request.json:
+        abort(404)
+
+    vendor_id = request.json.get('pk')
+    vendor = Vendor.query.filter_by(id=vendor_id)
+
+    vendor.update_field(request.json.get('key'), request.json.get('value'))
+
+    return Response(status=200, format='application/json')
