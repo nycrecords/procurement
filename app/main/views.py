6af8ac184af52fa492b74d02c35770721a3ea4f9
Matthew Laikhram <mlaikhram@AppDev-MBPR-801.local>
2017-01-17 11:21:31 -0500

    vendor information can now be used via dropdown

--- app/main/views.py
+++ app/main/views.py
@@ -25,6 +25,8 @@ def new_request():
     """Create a new procurement request."""
     form = RequestForm()
 
+    vendors = Vendor.query.all()
+
     if request.method == 'POST':
         if form.validate_on_submit():
             date_submitted = datetime.now()
@@ -45,41 +47,44 @@ def new_request():
                 newrequest = Request(form.request_name.data,
                                      date_submitted,
                                      form.item.data,
-                                 form.quantity.data, form.unit_price.data,
-                                 form.total_cost.data, form.funding_source.data,
-                                 form.funding_source_description.data, form.justification.data,
-                                 creator_id=current_user.id)
+                                     form.quantity.data, form.unit_price.data,
+                                     form.total_cost.data, form.funding_source.data,
+                                     form.funding_source_description.data, form.justification.data,
+                                     creator_id=current_user.id)
             request_vendor_name = str(form.request_vendor_name.data)
             request_vendor_phone = str(form.request_vendor_phone.data)
             request_vendor_fax = str(form.request_vendor_fax.data)
             request_vendor_mwbe = str(form.request_vendor_mwbe.data)
 
             newvendor = None
+            vendor_form = request.form["vendor"]
 
             if request_vendor_name != '':
                 if request_vendor_mwbe == "None":
                     request_vendor_mwbe = None
-
-                newvendor = Vendor(name=request_vendor_name,
-                                   address=form.request_vendor_address.data,
-                                   phone=request_vendor_phone,
-                                   fax=request_vendor_fax,
-                                   email=form.request_vendor_email.data,
-                                   tax_id=form.request_vendor_taxid.data,
-                                   mwbe=request_vendor_mwbe)
-                db.session.add(newvendor)
-                db.session.commit()
+                if vendor_form == "default":
+                    newvendor = Vendor(name=request_vendor_name,
+                                       address=form.request_vendor_address.data,
+                                       phone=request_vendor_phone,
+                                       fax=request_vendor_fax,
+                                       email=form.request_vendor_email.data,
+                                       tax_id=form.request_vendor_taxid.data,
+                                       mwbe=request_vendor_mwbe)
+                    db.session.add(newvendor)
+                    db.session.commit()
             else:
                 print(form.errors)
             if newvendor is not None:
                 newrequest.set_vendor_id(newvendor.id)
+            else:
+                newrequest.set_vendor_id(vendor_form)
             db.session.add(newrequest)
             db.session.commit()
-            return redirect(url_for('request.display_request'))
+            return redirect(url_for('request.display_request', request_id=newrequest.id))
         else:
             print(form.errors)
 
-    return render_template('main/new_request.html', form=form, user=current_user)
+    return render_template('main/new_request.html', form=form, user=current_user, vendors=vendors)
 
 
 @main.route('/divisions', methods=['GET'])
@@ -95,3 +100,11 @@ def divisions():
                 }
 
     return jsonify(divisions)
+
+
+@main.route('/parse_vendor', methods=['GET'])
+def jsonify_fields():
+    if request.args['vendor'] == "default":
+        return jsonify("")
+    v = Vendor.query.filter_by(id=request.args['vendor']).first()
+    return jsonify(v.name, v.address, v.phone, v.fax, v.email, v.tax_id, v.mwbe)
