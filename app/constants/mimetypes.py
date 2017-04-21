5f3f8298692a7b320a6ab7823f21c581537d8ba5
Matthew Laikhram <mlaikhram@MLAI-B10-0801-M.local>
2017-04-21 15:10:28 -0400

    gif is now an allowed mimetype

--- app/constants/mimetypes.py
+++ app/constants/mimetypes.py
@@ -8,5 +8,5 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf',
                           'doc', 'dot', 'docx', 'dotx', 'docm', 'dotm',
                           'xls', 'xlt', 'xla', 'xlsx', 'xltx', 'xlsm', 'xltm', 'xlam', 'xlsb',
                           'ppt', 'pot', 'pps', 'ppa', 'pptx', 'potx', 'ppsx', 'ppam', 'pptm', 'potm', 'ppsm',
-                          'png', 'jpg', 'jpeg'
+                          'png', 'jpg', 'jpeg', 'gif'
                           ])
